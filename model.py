e4564ddaa7a2cb3a04559972c2143480ea3fdae0
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-05-19 23:09:29 +0530

    initial commit

---
+++ model.py
@@ -0,0 +1,6 @@
+# model.py
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+
+# Load the pre-trained MobileNetV2 model
+model = MobileNetV2(weights='imagenet')
